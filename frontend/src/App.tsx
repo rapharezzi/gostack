2f2c501cf3f87e7284f87efe8248c181bb4eac5e
raphael rezzieri <rezzieri@gmail.com>
2020-09-17 20:41:59 -0400

    npm install

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return (
-    <h1>hello</h1>
-  );
-}
+const App: React.FC = () => <h1>hello world</h1>;
 
 export default App;
